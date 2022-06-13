7f821bb694b7eaeedf9d8fb70279fd1d3c397127
Egil Skjelbred <egil@skjelbred-design.no>
2022-06-13 14:20:14 +0200

    upload new styling

---
+++ egil/js/script.js
@@ -0,0 +1,13 @@
+const navHeader = document.querySelector("nav");
+
+navHeader.innerHTML = `        
+    <div class="navigation-logo"><a href="index.html" class="brand textdecoration-none">Weber & Johannesen Fysioterapi</a></div>
+        <div class="navigation-menu">
+            <ul>
+                <li><a class="textdecoration-none" href="om-oss.html">Om oss</a></li>
+                <li><a class="textdecoration-none" href="tjenester.html">Tjenester</a></li>
+                <li><a class="textdecoration-none" href="kontakt-oss.html">Kontakt oss</a></li>
+            </ul>
+        </div>
+    </div>
+`;
