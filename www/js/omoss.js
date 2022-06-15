e7db48c3e25ee4f23370be2e8c9904fe2348fe39
Egil Skjelbred <egil@skjelbred-design.no>
2022-06-15 14:34:32 +0200

    update design for further work on kontakt oss and om oss

--- www/js/omoss.js
+++ www/js/omoss.js
@@ -3,53 +3,41 @@
 
 // omOssArray med staffData starts
 const omossArray = {
-  employes: [
-    {
-      id: 1,
-      pictureUrl: "./img/TomAndreW.jpg",
-      imageAlt: "Terapeut-Tom",
-      name: "Tom Andre Weber",
-      title: "Fysioterapeut MSc og Manuellterapeut",
-      tlf: "414 26 858",
-      email: "tom@wjfysio.no",
-      active: true,
-    },
-    {
-      id: 2,
-      pictureUrl: "./img/Hanna-1-scaled-300x300.jpg",
-      imageAlt: "Terapeut-Hanna",
-      name: "Hanna Cortsen",
-      title: "Osteopat D.O MNOF",
-      tlf: "980 19 630",
-      email: "hanna@wjfysio.no",
-      active: true,
-    },
-    {
-      id: 3,
-      pictureUrl: "./img/thereseBG.png",
-      imageAlt: "Terapeut-Therese",
-      name: "Therese Johannesen",
-      title: "Fysioterapeut",
-      tlf: "999 77 666",
-      email: "therese@wjfysio.no",
-      active: true,
-    },
-    {
-      id: 4,
-      pictureUrl: "./img/nyEmplAvatarBG.png",
-      imageAlt: "Terapeut-ny",
-      name: "Terapeut-navn",
-      title: "TBA - skal være synelig?",
-      tlf: "999 77 666",
-      email: "nyEmplyee@wjfysio.no",
-      active: false,
-      //   set if in-aktive to hide object or just comment out?
-    },
-  ],
+    employes: [
+        {
+            id: 1,
+            pictureUrl: "./img/TomAndreW.jpg",
+            imageAlt: "Terapeut-Tom",
+            name: "Tom Andre Weber",
+            title: "Fysioterapeut MSc og Manuellterapeut",
+            tlf: "414 26 858",
+            email: "tom@wjfysio.no",
+            active: true,
+        },
+        {
+            id: 2,
+            pictureUrl: "./img/thereseBG.png",
+            imageAlt: "Terapeut-Therese",
+            name: "Therese Johannesen",
+            title: "Fysioterapeut",
+            tlf: "999 77 666",
+            email: "therese@wjfysio.no",
+            active: true,
+        },
+        {
+            id: 3,
+            pictureUrl: "./img/Hanna-1-scaled-300x300.jpg",
+            imageAlt: "Terapeut-Hanna",
+            name: "Hanna Cortsen",
+            title: "Osteopat D.O MNOF",
+            tlf: "980 19 630",
+            email: "hanna@wjfysio.no",
+            active: true,
+        },
+    ],
 };
 
 // ønsker , legge til bilder, om klinikken info
-
 
 // ref til classen og funskj starts
 
@@ -58,9 +46,9 @@ console.log(employes);
 const omossCards = document.querySelector(".staff-card-jS");
 
 function employesObjects() {
-  omossCards.innerHTML = ``;
-  for (let i = 0; i < employes.length; i++) {
-    omossCards.innerHTML += `
+    omossCards.innerHTML = ``;
+    for (let i = 0; i < employes.length; i++) {
+        omossCards.innerHTML += `
 
     <div class="herosection-content__flex"> 
       <img src="${employes[i].pictureUrl}" alt="${employes[i].imageAlt}">
@@ -70,7 +58,7 @@ function employesObjects() {
       <p>${employes[i].email}</p> 
     </div>
     `;
-  }
+    }
 }
 
 employesObjects();
--- www/js/script.js
+++ www/js/script.js
@@ -3,6 +3,10 @@ const navHeader = document.querySelector("nav");
 navHeader.innerHTML = `        
     <div class="navigation-logo"><a href="index.html" class="brand textdecoration-none">Weber & Johannesen Fysioterapi</a></div>
         <div class="navigation-menu">
+        <input type="checkbox" id="check" />
+        <label for="check" class="checkBtn">
+            <i class="material-icons">menu</i>
+        </label>
             <ul>
                 <li><a class="textdecoration-none" href="om-oss.html">Om oss</a></li>
                 <li><a class="textdecoration-none" href="tjenester.html">Tjenester</a></li>
@@ -11,3 +15,28 @@ navHeader.innerHTML = `
         </div>
     </div>
 `;
+
+const navFooter = document.querySelector("footer");
+
+navFooter.innerHTML = `
+© 2022 WEBER & JOHANNESEN FYSIOTERAPI AS
+
+`;
+// navFooter.innerHTML = `
+//     <div class="">
+//         <h3>Kontaktinfo:</h3>
+//             <p><a href="tel:41426858" class="textdecoration-none">414 26 858</a></p>
+//             <p><a href="mailto:tom@wjfysio.no" class="textdecoration-none">tom@wjfysio.no</a></p>
+//         </div>
+//         <div class="">
+//             <h3>Åpningstider:</h3>
+//             <p>Mandag - Fredag</p>
+//             <p>08:00 - 16:00</p>
+//         </div>
+//         <div class="">
+//             <h3>Adresse:</h3>
+//             <p>Håkestadveien 3</p>
+//             <p>3280 Tjøllingvollen</p>
+//         </div>
+
+// `;
